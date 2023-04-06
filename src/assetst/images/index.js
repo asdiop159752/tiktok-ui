657eff00e5090c17e1bd0e869510a0f15fec693a
asdiop159752 <nxt159753@gmail.com>
2023-04-06 18:04:52 +0700

    UI Header

---
+++ src/assetst/images/index.js
@@ -0,0 +1,5 @@
+const images = {
+logo: require('~/assetst/images/logo.svg').default
+
+}
+export default images
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,12 +1,16 @@
-import Header from './Header'
+import Header from '~/components/Layout/components/Header'
 import Sidebar from './Sidebar'
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss'
 
+const cx = classNames.bind(styles)
 function DefaultLayout({children}) {
-    return ( <div> 
+    return ( 
+    <div className={cx('wrapper')}> 
         <Header />
-        <div className='container'>
+        <div className={cx('container')}>
         <Sidebar />
-            <div classname='content' >{children} </div>
+            <div className={cx('content')} >{children} </div>
         </div>
     </div> 
     );
