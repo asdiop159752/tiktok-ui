33f666d555f6b3bf4de876c9e5810485e1ee648a
asdiop159752 <nxt159753@gmail.com>
2023-04-07 16:29:57 +0700

    Xây dựng UI phần 2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,22 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img className={cx('avatar')} src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/a9080a6a0c285c4631594e331e843509~c5_300x300.webp?x-expires=1681030800&x-signature=2oCSm9O28UXNm7IEQtywT4F7O1Y%3D" alt="Hoa" />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span> Nguyen Van Hoa</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}> nguyenvanhoa</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
